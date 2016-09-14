b96db6e8e00e2c6ba6652920e14c308178694031
Alon Goldboim <alongoldboim@gmail.com>
2016-09-14 11:41:41 +0300

    Auto fill serialized value

--- app/models/custom_attribute.rb
+++ app/models/custom_attribute.rb
@@ -2,6 +2,10 @@ class CustomAttribute < ApplicationRecord
   belongs_to :resource, :polymorphic => true
   serialize :serialized_value
 
+  def value=(value)
+    self.serialized_value = value
+  end
+
   def stored_on_provider?
     source == "VC"
   end
