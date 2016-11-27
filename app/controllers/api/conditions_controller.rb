320a9474165d7d9adfd1ddd719117fa4cf7b1b20
Alon Goldboim <alongoldboim@gmail.com>
2016-11-27 14:44:01 +0200

    add_exp_creation_on_condition_api

--- app/controllers/api/conditions_controller.rb
+++ app/controllers/api/conditions_controller.rb
@@ -1,4 +1,19 @@
 module Api
   class ConditionsController < BaseController
+    def create_resource(type, _id, data = {})
+      assert_id_not_specified(data, type)
+      data["expression"] = MiqExpression.new(data["expression"])
+      super(type, _id, data)
+    rescue => err
+      raise BadRequestError, "Cant create condition: #{err}"
+    end
+
+    def edit_resource(type, id = nil, data = {})
+      raise BadRequestError, "Must specify an id for editing a #{type} resource" unless id
+      data["expression"] = MiqExpression.new(data["expression"]) if data["expression"]
+      super(type, id, data)
+    rescue => err
+      raise BadRequestError, "Cant edit condition: #{err}"
+    end
   end
 end
